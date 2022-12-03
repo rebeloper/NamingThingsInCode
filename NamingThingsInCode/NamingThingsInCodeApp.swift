2fb8d464155dba1d4150edcf22be8c48bbf5b84f
rebeloper <therebeloper@gmail.com>
2022-12-03 05:13:53 +0200

    Initial Commit

---
+++ NamingThingsInCode/NamingThingsInCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NamingThingsInCodeApp.swift
+//  NamingThingsInCode
+//
+//  Created by Alex Nagy on 03.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct NamingThingsInCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
